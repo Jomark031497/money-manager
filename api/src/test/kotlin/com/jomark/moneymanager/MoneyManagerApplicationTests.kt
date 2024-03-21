a40afa33feb8d0570078d7c5e22610085a7d7406
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-03-21 21:42:31 +0800

    initialized kotlin + spring boot using spring initializr

---
+++ api/src/test/kotlin/com/jomark/moneymanager/MoneyManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jomark.moneymanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoneyManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
